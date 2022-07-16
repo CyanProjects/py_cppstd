fff585c3bf963c6e7e225408d479068ec8b5c9ca
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-16 22:02:59 +0800

    Update test: main.py

--- py_cppstd/main.py
+++ py_cppstd/main.py
@@ -1,7 +1,9 @@
 from py_cppstd import std
-std.load()
-std.include('iostream')
-std.cout << "enter sth. here\n"
-sth: str = ''
-std.cin >> sth
-std.cout << sth << std.endl
+
+if __name__ == '__main__':
+    std = std()
+    std.include('iostream')
+    std.cout << "enter sth. here\n"
+    sth: str = ''
+    std.cin >> sth
+    std.cout << sth << std.endl

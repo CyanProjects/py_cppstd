435c10224df28feba7fb8af71ee5413586ff7d1a
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-16 20:49:38 +0800

    Upload package to Pypi

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description=[
         'Using cpp std iostream lib in py'
     ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author='Cyan_Changes',
     author_email='1491326171@qq.com',
     maintainer='Cyan_Changes',
@@ -13,6 +15,13 @@ setup(
     license='Apache 2.0',
     packages=find_packages(),
     platforms=['all'],
-    url='https://github.com/Chinese-Cyq20100313/py_cppstd'
-    
+    url='https://github.com/Chinese-Cyq20100313/py_cppstd',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Printing"
+    ]
 )

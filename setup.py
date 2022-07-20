b4bb51fcb6d34b4fb43741d471da4c3858aae5ac
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-20 13:18:34 +0800

    Update version code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py_cppstd',
-    version='0.2',
+    version='0.2.1',
     description=[
         'Using cpp std iostream lib in py'
     ],

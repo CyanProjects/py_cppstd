27fcb55e4fbe2f7a7518f1dc363635f6552bb343
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-20 11:28:12 +0800

    Fix Workflow test problem

--- tests/test_use.py
+++ tests/test_use.py
@@ -1,36 +1,24 @@
 import unittest
-
+import py_cppstd as cppstd
+from py_cppstd import exceptions
 
 class TestStd(unittest.TestCase):
-    def test_init(self):
-        from ..py_cppstd import std
-
-    def test_loads(self):
-        from ..py_cppstd import std
-        tp_std = type(std)
-        std.load()
-        del std
-        import py_cppstd
-        py_cppstd.std.load()
-
     def test_wrong_includes(self):
-        from ..py_cppstd import std, exceptions
+        std = cppstd.std()
         with self.assertRaises(exceptions.NoLibraryError):
             std.include('iosteam')
             std.include('stdbuggy')
 
     def test_includes(self):
-        from ..py_cppstd import std
-        std = std()
+        std = cppstd.std()
         std.include('iostream')
         self.assertIsInstance(std, object)
 
     def test_iostream(self):
-        from ..py_cppstd import std
-        std = std()
+        std = cppstd.std()
         std.include('iostream')
-        self.assertIs(std.cout << 114514 << '\n', std.cout) # Test integer
-        self.assertIs(std.cout << '114514', std.cout) # Test string
+        self.assertIs(std.cout << 114514 << '\n', std.cout)  # Test integer
+        self.assertIs(std.cout << '114514', std.cout)  # Test string
 
 
 if __name__ == '__main__':

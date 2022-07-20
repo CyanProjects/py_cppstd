9084293530147d1babd61f603bf995f37f3afe19
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-20 13:16:21 +0800

    try Fix workflow tests problem

--- tests/test_use.py
+++ tests/test_use.py
@@ -1,21 +1,20 @@
 import unittest
-import py_cppstd as cppstd
-from py_cppstd import exceptions
+from py_cppstd import std as cppstd, exceptions
 
 class TestStd(unittest.TestCase):
     def test_wrong_includes(self):
-        std = cppstd.std()
+        std = cppstd()
         with self.assertRaises(exceptions.NoLibraryError):
             std.include('iosteam')
             std.include('stdbuggy')
 
     def test_includes(self):
-        std = cppstd.std()
+        std = cppstd()
         std.include('iostream')
         self.assertIsInstance(std, object)
 
     def test_iostream(self):
-        std = cppstd.std()
+        std = cppstd()
         std.include('iostream')
         self.assertIs(std.cout << 114514 << '\n', std.cout)  # Test integer
         self.assertIs(std.cout << '114514', std.cout)  # Test string

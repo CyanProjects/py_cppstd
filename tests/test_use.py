d5989251fd10d77633744d22e34524fb62114dd4
Chinese-Cyq20100313 <1491326171@qq.com>
2022-07-20 11:19:42 +0800

    Fix Workflow test problem

--- tests/test_use.py
+++ tests/test_use.py
@@ -3,10 +3,10 @@ import unittest
 
 class TestStd(unittest.TestCase):
     def test_init(self):
-        from py_cppstd import std
+        from ..py_cppstd import std
 
     def test_loads(self):
-        from py_cppstd import std
+        from ..py_cppstd import std
         tp_std = type(std)
         std.load()
         del std
@@ -14,19 +14,19 @@ class TestStd(unittest.TestCase):
         py_cppstd.std.load()
 
     def test_wrong_includes(self):
-        from py_cppstd import std, exceptions
+        from ..py_cppstd import std, exceptions
         with self.assertRaises(exceptions.NoLibraryError):
             std.include('iosteam')
             std.include('stdbuggy')
 
     def test_includes(self):
-        from py_cppstd import std
+        from ..py_cppstd import std
         std = std()
         std.include('iostream')
         self.assertIsInstance(std, object)
 
     def test_iostream(self):
-        from py_cppstd import std
+        from ..py_cppstd import std
         std = std()
         std.include('iostream')
         self.assertIs(std.cout << 114514 << '\n', std.cout) # Test integer

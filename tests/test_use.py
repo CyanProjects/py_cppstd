61b8493f4fc1dfbece9b06d8966c2a003cb627e4
Cyan <68551684+Chinese-Cyq20100313@users.noreply.github.com>
2023-01-15 19:29:02 +0800

    Update tests

--- tests/test_use.py
+++ tests/test_use.py
@@ -12,18 +12,15 @@ from py_cppstd import std as cppstd, exceptions
 
 class TestStd(unittest.TestCase):
     def test_wrong_includes(self):
-        std = cppstd()
-        with self.assertRaises(exceptions.NoLibraryError):
+        with self.assertRaises(exceptions.NoSuchLibraryError):
             std.include('iosteam')
             std.include('stdbuggy')
 
     def test_includes(self):
-        std = cppstd()
         std.include('iostream')
         self.assertIsInstance(std, object)
 
     def test_iostream(self):
-        std = cppstd()
         std.include('iostream')
         self.assertIs(std.cout << 114514 << '\n', std.cout)  # Test integer
         self.assertIs(std.cout << '114514', std.cout)  # Test string
